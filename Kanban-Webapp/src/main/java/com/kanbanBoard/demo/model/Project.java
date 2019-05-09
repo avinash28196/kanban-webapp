214cc239bd7441e0d75efa749f3529250853001b
Avinash Ramesh <avinash28196@gmail.com>
2019-05-09 23:56:45 +0530

    Curd ops on Projects

--- Kanban-Webapp/src/main/java/com/kanbanBoard/demo/model/Project.java
+++ Kanban-Webapp/src/main/java/com/kanbanBoard/demo/model/Project.java
@@ -9,34 +9,44 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @SuppressWarnings("serial")
 @Entity
+@EnableJpaAuditing
+
 public class Project extends AuditModel{
 	
 	
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 	
 	private String name;
 	
 	private String description;
-	
-	
+		
 	
 	public Project() {
 
 	}
-	
+		
+
 	
 
-	public int getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+
+
+
+	public void setId(Long id) {
 		this.id = id;
 	}
+
+
+
 
 	public String getName() {
 		return name;
